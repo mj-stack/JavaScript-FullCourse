83de926920f5b32d71f382e18f795b3649bd2bc3
Mukul Joshi <mjoshi1819@gmail.com>
2025-01-17 19:02:34 +0530

    JavaScript Basics

---
+++ Lesson-01 JavaScript Basics/lesson01.js
@@ -0,0 +1,27 @@
+alert('Good morning!');
+
+alert('Good morning!');
+
+10 + 5
+15
+
+20 - 5
+15
+
+2 + 2 - 5
+-1
+
+document.body.innerHTML = 'Good Morning';
+'Good Morning'
+
+document.body.innerHTML = 'Mukul Joshi';
+'Mukul Joshi'
+
+10 + 8 + 20
+38
+
+(100 - 20 - 50) + 200
+230
+
+document.body.innerHTML = '';
+''
